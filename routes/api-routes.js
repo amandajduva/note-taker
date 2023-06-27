29b493838c30e570c78d47b9c49e3d52ace56158
Amanda Duva <amanda.j.duva@gmail.com>
2023-06-27 10:53:16 -0400

    added notes

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -1,20 +1,25 @@
+// Imports
 const router = require("express").Router();
 const { readFromFile, readAndAppend, readAndDelete } = require("../helpers/fsUtils");
 const uuid = require("../helpers/uuid");
 
+// Get json data
 router.get("/notes", (req, res) => {
     readFromFile("./db/db.json").then(data => res.json(JSON.parse(data)))
-})
+});
 
+// Post new note
 router.post("/notes", (req, res) => {
     let newNote = { title: req.body.title, text:req.body.text, id:uuid() }
     readAndAppend(newNote, "./db/db.json")
     res.json("success")
-})
+});
 
+// Delete note
 router.delete("/notes/:id", (req, res) => {
     readAndDelete(req.params.id, "./db/db.json")
     res.json("success")
-})
+});
 
+// export
 module.exports = router
